83ffebc0bc1ccdf525b46eb3aef27d9faed3303b
m4rc310 <mlsilva@coamo.com.br>
2024-10-03 23:26:17 -0300

    change weather

--- src/main/java/br/com/m4rc310/persona/api/dto/weather/DtoHourlyWeather.java
+++ src/main/java/br/com/m4rc310/persona/api/dto/weather/DtoHourlyWeather.java
@@ -2,13 +2,14 @@ package br.com.m4rc310.persona.api.dto.weather;
 
 import java.io.Serializable;
 
+import br.com.m4rc310.persona.api.i18n.IConsts;
 import br.com.m4rc310.weather.dto.MWeatherHourly;
 import io.leangen.graphql.annotations.GraphQLIgnore;
 import io.leangen.graphql.annotations.types.GraphQLType;
 import lombok.Data;
 
 @Data
-@GraphQLType(name = "${type.hourly.weather}")
+@GraphQLType(name = IConsts.TYPE$hourly_weather, description = IConsts.DESC$type_hourly_weather)
 public class DtoHourlyWeather implements Serializable {
 	private static final long serialVersionUID = 6814409592238202749L;
 	
